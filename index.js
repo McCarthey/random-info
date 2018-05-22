c4f27ece77c874d9fff1a317f2c1131dd79856ea
Dean <mccarthey123@outlook.com>
2018-05-22 14:45:19 +0800

    文件路径

--- index.js
+++ index.js
@@ -17,14 +17,17 @@ for (let i = 0; i < length; i++) {
 
 let content = JSON.stringify(result)
 console.log(content)
-fs.writeFile("test.js", `export const random_info = ${content}`, err => {
-	if(err){
-		return console.error(err)
-	}
-	fs.readFile("test.js",(err,data)=>{
-		if(err){
-			return console.error(err)
-		}
-		console.log(`异步读取文件数据: ${data}`)
-	})
+// 要写入/修改的文件路径
+const path = "../SPA/lemo_card/src/entries/wx_lemo_card/constant/random_info.js"
+
+fs.writeFile(path, `export const random_info = ${content}`, err => {
+    if (err) {
+        return console.error(err)
+    }
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            return console.error(err)
+        }
+        console.log(`异步读取文件数据: ${data}`)
+    })
 })
